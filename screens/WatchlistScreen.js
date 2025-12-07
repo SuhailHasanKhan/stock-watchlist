1a79d9ac3059b49b7f44b9ab739cc61a7209e915
Suhail Hasan Khan <shasankhan@usf.edu>
2025-12-07 08:51:15 -0500

    Started working on the Watchlist screen function.

---
+++ screens/WatchlistScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  FlatList,
+  StyleSheet,
+  Button,
+} from 'react-native';
+import StockListItem from '../components/StockListItem';
+
+const PRIMARY = '#00b289';
+const TEXT_DARK = '#1f2933';
+const TEXT_MUTED = '#7b8794';
+const SCREEN_BG = '#f5f7fa';
+const CARD_BG = '#ffffff';
