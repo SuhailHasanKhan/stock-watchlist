f8eabfbaeb8eed7026beecfdec767de2809d3a7a
Suhail Hasan Khan <shasankhan@usf.edu>
2025-12-07 08:29:01 -0500

    Started working on the editing stock screen

--- screens/AddStockScreen.js
+++ screens/AddStockScreen.js
@@ -54,3 +54,4 @@ export default function AddStockScreen({ navigation, onAddStock }) {
     </View>
   );
 }
+
---
+++ screens/EditStockScreen.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  Button,
+  StyleSheet,
+  Alert,
+} from 'react-native';
+
+const PRIMARY = '#00b289';
+const TEXT_DARK = '#1f2933';
+const TEXT_MUTED = '#7b8794';
+const SCREEN_BG = '#f5f7fa';
+const CARD_BORDER = '#d0d7e2';
