3ec82781409ca4d4f805572867a11ffee3ff32bb
Suhail Hasan Khan <shasankhan@usf.edu>
2025-12-07 09:22:07 -0500

    Started creating the actual app page.

--- App.js
+++ App.js
@@ -1,20 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import WatchlistScreen from './screens/WatchlistScreen';
+import AddStockScreen from './screens/AddStockScreen';
+import EditStockScreen from './screens/EditStockScreen';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
