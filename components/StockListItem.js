446f352630939c1030999aa8007582e1fd994cd8
Suhail Hasan Khan <shasankhan@usf.edu>
2025-12-07 09:04:58 -0500

    started working on my component.

---
+++ components/StockListItem.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+
+const PRIMARY = '#00b289';
+const TEXT_DARK = '#1f2933';
+const TEXT_MUTED = '#7b8794';
+const CARD_BG = '#ffffff';
